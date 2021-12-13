1010c0708290385c18f9079a8827d23c14eda0f1
AntonyMorgante <antonymorgante1@gmail.com>
2021-12-13 14:58:49 +0100

    comments

--- js/myscript.js
+++ js/myscript.js
@@ -1,15 +1,19 @@
+/* lista delle mail in rubrica */
 const usermails = ("tizio@mail.it", "caio@mail.it", "sempronio@mail.it")
 
+/* chiediamo la mail all'utente */
 const msMail = prompt("Inserisci la tua mail")
 
+/* creiamo una variabile di verifica */
 let verified = false;
 
-for (i=0; i<=usermails.length; i++){
-    if (msMail == usermails[i]){
-        verified = true;
+for (i=0; i<=usermails.length; i++){ /* Controlliamo su tutte le mail in rubrica */
+    if (msMail == usermails[i]){ /* verifichiamo se la mail inserita dall'utente è presente in rubrica */
+        verified = true; /* in caso affermativo la nostra variabile di verifica risulta vera, altrimenti rimarrà falsa */
     }
 }
 
+/* A seconda del valore della variabile di verifica stampiamo su schermo il risultato */
 if (verified){
     document.writeln("la tua mail non compare nell'elenco");
 }
--- js/myscript2.js
+++ js/myscript2.js
@@ -1,7 +1,10 @@
-const firstDice = Math.floor(Math.random()*6+1)
-const secondDice = Math.floor(Math.random()*6+1)
+const firstDice = Math.floor(Math.random()*6+1) /* valore del primo dado */
+const secondDice = Math.floor(Math.random()*6+1) /* valore del secondo dado */
 
+/* Scriviamo il valore dei due dadi */
 document.writeln("Il tuo lancio di dado vale " + firstDice + ", mentre il lancio di dado del computer vale " + secondDice + ".")
+
+/* confronto tra valori ed esito */
 if (firstDice > secondDice){
     document.writeln("Congratulazioni! Hai vinto!");
 }
